c70cde8f30c3c9479cfbc3fbc4e4c692226f3f98
nguyenhoangtin291 <nguyenhoangtin291@gmail.com>
2025-06-29 16:40:17 -0500

    update code

---
+++ LibrarySolution/Library.Application/Members/Login/LoginCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Library.Application.Members.Login;
+
+public class LoginCommand : IRequest<string>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string? RefreshToken { get; set; }
+}
---
+++ LibrarySolution/Library.Application/Members/Login/LoginCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Application.Members.Login;
+
+public class LoginCommandHandler : IRequestHandler<LoginCommand, string>
+{
+    public async Task<string> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        // Get member
+
+        // Check password
+
+        // Check if refresh token is valid
+
+        // Generate new refresh token
+        // Generate new access token
+        // Return access token
+        throw new NotImplementedException();
+    }
+}
